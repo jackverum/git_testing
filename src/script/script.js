5950ec2776f98f96684f75bab356a25941ff3336
Jack <testgit@gmail.com>
2020-12-10 00:42:08 +0200

    update template to normal cond

---
+++ src/script/script.js
@@ -0,0 +1,19 @@
+console.log("git test");
+
+/*
+npm init
+
+npm install gulp-cli -g              : global
+npm install gulp-cli --save-dev      : local
+
+npm install --global gulp-cli       :2020
+npm install --save-dev gulp         :2020
+
+npm i gulp-sass --save-dev
+npm i browser-sync --save-dev
+npm i gulp-autoprefixer --save-dev
+npm i gulp-clean-css --save-dev
+npm i gulp-cli --save-dev
+
+
+*/
